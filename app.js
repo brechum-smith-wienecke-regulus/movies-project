9d936d6e1c8b0c05ee0215f37a7c5a5ddc990ff8
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-10 14:15:33 -0600

    form for adding movies created

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 "use strict";
 const movieAPI= "https://pushy-paint-hippopotamus.glitch.me/movies"
-const options= {
+
+const movieDisplay = $("#movie-display")
+const getMovies = () =>
+{const options= {
     method: 'GET',
     headers: {
         "Content-Type": "application/json",
     }
 }
-const getMovies = ()
-{
     fetch(movieAPI, options)
         .then(response => response.json())
         .then(movies => {
+            hideLoading()
             movies.forEach(movie => {
+                movieDisplay.append(renderMovie(movie))
                 console.log(movie);
             })
         })
+}
+getMovies();
+
+const hideLoading = () => {
+    $("#loading").hide();
+}
+
+const renderMovie = (movie) => {
+    let movieHtml = `<h1>${movie.title}</h1>`
+    return movieHtml
 }
