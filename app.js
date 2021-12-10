89ea56bb671914e678f4ab27a76e152093224e5d
Eva Brechum <Brechum.Eva@gmail.com>
2021-12-10 14:48:00 -0600

    working on movies project

--- app.js
+++ app.js
@@ -29,3 +29,27 @@ const renderMovie = (movie) => {
     let movieHtml = `<h1>${movie.title}</h1>`
     return movieHtml
 }
+
+const addMovie = (rating, title) => {
+    const movie = {
+        title: title,
+        rating: rating,
+        director: "",
+        year: "",
+        genre: "",
+        poster: "",
+        plot: "",
+        actors: "",
+        id: "",
+    }
+    const options = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(movie)
+    }
+    console.log(movie);
+    return fetch(movieAPI, options).then(response => response.json())
+}
+
