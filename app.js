0a2cf066f8530f72bacfd34d1e4b1f63ed064e85
James Wienecke <james.a.wienecke@gmail.com>
2021-12-13 11:59:09 -0600

    Starting to implement a sorting system.

--- app.js
+++ app.js
@@ -357,6 +357,29 @@ $(document).ready(() => {
           });
     }
 
+    const dynamicSort = (property) => {
+        var sortOrder = 1;
+
+        if(property[0] === "-") {
+            sortOrder = -1;
+            property = property.substr(1);
+        }
+
+        return function (a,b) {
+            if(sortOrder == -1){
+                return b[property].localeCompare(a[property]);
+            }else{
+                return a[property].localeCompare(b[property]);
+            }
+        }
+    }
+
+    const sortMovieProperties = () => {
+        $('#sort-select').on('change', function () {
+            // dynamicSort($(this).val())
+        });
+    }
+
     // all actual work done that is not simply function definitions should go in here to keep organized :)
     (() => {
         // grab movies and do setup work as soon as page loads
